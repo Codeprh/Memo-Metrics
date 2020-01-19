6fd51ef47f1798c13488558f8e7f85ed1aa4c9d8
Noah Pan <codingprh@gmail.com>
2020-01-19 14:23:23 +0800

    memo-gateway初始化

---
+++ memo-gateway/src/main/java/noah/memo/memogateway/MemoGatewayApplication.java
@@ -0,0 +1,13 @@
+package noah.memo.memogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoGatewayApplication.class, args);
+    }
+
+}
---
+++ memo-gateway/src/test/java/noah/memo/memogateway/MemoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package noah.memo.memogateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
