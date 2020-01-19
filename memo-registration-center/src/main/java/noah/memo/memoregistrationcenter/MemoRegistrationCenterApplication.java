f08d4bdd8bb0a8f3aabcd5e001db898901775e00
Noah Pan <codingprh@gmail.com>
2020-01-19 18:06:40 +0800

    memo-registration-center初始化

---
+++ memo-registration-center/src/main/java/noah/memo/memoregistrationcenter/MemoRegistrationCenterApplication.java
@@ -0,0 +1,13 @@
+package noah.memo.memoregistrationcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoRegistrationCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoRegistrationCenterApplication.class, args);
+    }
+
+}
---
+++ memo-registration-center/src/test/java/noah/memo/memoregistrationcenter/MemoRegistrationCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package noah.memo.memoregistrationcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoRegistrationCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
