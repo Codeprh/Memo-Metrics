0df410316708fd1566a49cc0d42e02ad9c3d9c0e
Noah Pan <codingprh@gmail.com>
2020-01-21 10:39:42 +0800

    eureka和apollo整合完成

--- memo-registration-center/src/main/java/noah/memo/memoregistrationcenter/MemoRegistrationCenterApplication.java
+++ memo-registration-center/src/main/java/noah/memo/memoregistrationcenter/MemoRegistrationCenterApplication.java
@@ -1,9 +1,13 @@
 package noah.memo.memoregistrationcenter;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableApolloConfig
 public class MemoRegistrationCenterApplication {
 
     public static void main(String[] args) {
