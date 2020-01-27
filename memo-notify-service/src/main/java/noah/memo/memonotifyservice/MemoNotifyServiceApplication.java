226a1db27dbc31499c41be083612ca7382dfbac4
Noah Pan <codingprh@gmail.com>
2020-01-28 06:45:37 +0800

    第一版本各个模块

---
+++ memo-notify-service/src/main/java/noah/memo/memonotifyservice/MemoNotifyServiceApplication.java
@@ -0,0 +1,13 @@
+package noah.memo.memonotifyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoNotifyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoNotifyServiceApplication.class, args);
+    }
+
+}
---
+++ memo-notify-service/src/test/java/noah/memo/memonotifyservice/MemoNotifyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package noah.memo.memonotifyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoNotifyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
