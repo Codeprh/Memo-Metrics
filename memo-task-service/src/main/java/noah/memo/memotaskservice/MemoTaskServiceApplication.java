6ed5fc67d8903424a3a9e8967360575b117740ef
Noah Pan <codingprh@gmail.com>
2020-01-28 06:40:20 +0800

    task模板

---
+++ memo-task-service/src/main/java/noah/memo/memotaskservice/MemoTaskServiceApplication.java
@@ -0,0 +1,13 @@
+package noah.memo.memotaskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoTaskServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoTaskServiceApplication.class, args);
+    }
+
+}
---
+++ memo-task-service/src/test/java/noah/memo/memotaskservice/MemoTaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package noah.memo.memotaskservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoTaskServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
