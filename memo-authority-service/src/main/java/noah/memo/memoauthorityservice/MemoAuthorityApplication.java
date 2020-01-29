f08cd23b7e0e0def220116634214fcbe2f79c557
Noah Pan <codingprh@gmail.com>
2020-01-29 11:50:41 +0800

    配置文件

--- memo-authority-service/src/main/java/noah/memo/memoauthorityservice/MemoAuthorityApplication.java
+++ memo-authority-service/src/main/java/noah/memo/memoauthorityservice/MemoAuthorityApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
@@ -21,7 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EntityScan("noah.memo")
 public class MemoAuthorityApplication {
 
-    @RequestMapping(method = RequestMethod.GET, name = "/hello")
+    @GetMapping(name = "/hello")
     @ResponseBody
     public String hello() {
         return "hello";
