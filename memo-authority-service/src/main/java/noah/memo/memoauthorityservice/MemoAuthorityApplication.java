6afb7f4d8aeeb0d81d07f3823d574c0c1d91d984
Noah Pan <codingprh@gmail.com>
2020-01-29 10:00:20 +0800

    更新maven依赖

--- memo-authority-service/src/main/java/noah/memo/memoauthorityservice/MemoAuthorityApplication.java
+++ memo-authority-service/src/main/java/noah/memo/memoauthorityservice/MemoAuthorityApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableApolloConfig
+@EnableJpaRepositories(basePackages = {"noah.memo"})
 public class MemoAuthorityApplication {
 
     public static void main(String[] args) {
--- memo-registration-center/src/main/java/noah/memo/memoregistrationcenter/MemoRegistrationCenterApplication.java
+++ memo-registration-center/src/main/java/noah/memo/memoregistrationcenter/MemoRegistrationCenterApplication.java
@@ -3,9 +3,10 @@ package noah.memo.memoregistrationcenter;
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 @EnableApolloConfig
 public class MemoRegistrationCenterApplication {
