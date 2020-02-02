c2ccc2e320f6db63dac9033893a641ede556c1be
chenyuhao <chenyuhao@guorou.tv>
2020-02-02 15:51:03 +0800

    更新Feign调用失败的问题。 1、修改Feign包名扫描。 2、因为server.servlet.context-path的影响，feign增加path参数。

--- memo-authority-api/src/main/java/noah/memo/memoauthorityapi/AuthorityApi.java
+++ memo-authority-api/src/main/java/noah/memo/memoauthorityapi/AuthorityApi.java
@@ -1,6 +1,8 @@
 package noah.memo.memoauthorityapi;
 
+import feign.hystrix.FallbackFactory;
 import noah.memo.memoauthorityapi.bean.Account;
+import noah.memo.memoframework.log.Logger;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
  *
  * @author noah
  */
-@FeignClient(name = AuthorityApi.SERVICE_NAME, fallback = AuthorityApi.DefaultAuthorityApiFallback.class)
+@FeignClient(name = AuthorityApi.SERVICE_NAME, fallback = AuthorityApi.DefaultAuthorityApiFallback.class, path = "authoritys")
 public interface AuthorityApi {
 
     String SERVICE_NAME = "authority-service";
@@ -41,6 +43,20 @@ public interface AuthorityApi {
         @Override
         public String hello() {
             System.out.println("hello调用失败");
+            return null;
+        }
+    }
+
+
+    /**
+     * fallback 与fallbackFactory不能同时使用
+     */
+    //    @Component
+    class DefaultAuthorFallbackFactory implements FallbackFactory<AuthorityApi> {
+
+        @Override
+        public AuthorityApi create(Throwable throwable) {
+            Logger.error("Fallback Error", throwable);
             return null;
         }
     }
